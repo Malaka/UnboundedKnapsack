647f6d630fe13e7b676bd0e96dee4ce9b2439840
Malaka Ekanayake <Malaka.Mudiyanselage@originenergy.com.au>
2019-05-04 16:25:00 +1000

    added test cases

--- src/test/java/com/au/anz/algo/test/unboundedKnapsack/DynamicProgrammingTest.java
+++ src/test/java/com/au/anz/algo/test/unboundedKnapsack/DynamicProgrammingTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class RecursiveTest {
+public class DynamicProgrammingTest {
 
     @Test
     public void testWithData() {
-        Algo recursive = new Recursive();
+        Algo recursive = new DynamicProgramming();
         assertEquals("base case", 0, (int) recursive.knapsackValue(Stream.of(2).toArray(Integer[]::new), 0));
         assertEquals(4, (int) recursive.knapsackValue(Stream.of(2).toArray(Integer[]::new), 4));
         assertEquals(21, (int) recursive.knapsackValue(Stream.of(3, 6, 7).toArray(Integer[]::new), 21));
